2ec70de852d0bab965e370e7806a6438f0fc469d
Vladimir Vivien <vladimir.vivien@gmail.com>
2023-03-19 15:13:58 +0000

    Implements an informer for metrics-api types
    This patch is for improving scalability of metrics data.
    It does that by implementing an informer/lister for the
    metrics-api data. This change will  cache metrics data
    and avoid retrieving metrics data with directly from the
    api-server, with each update.
    
    Other addition includes
    - Files for scale testing with project KWOK

--- hack/minikube.sh
+++ hack/minikube.sh
@@ -2,6 +2,6 @@
 
 minikube start --nodes 2 --memory=4096 --cpus=2 \
   --kubernetes-version=v1.23.1 \
-  --vm-driver=hyperkit \
+  --vm-driver=docker \
   --bootstrapper=kubeadm \
   --extra-config=apiserver.enable-admission-plugins="LimitRanger,NamespaceExists,NamespaceLifecycle,ResourceQuota,ServiceAccount,DefaultStorageClass,MutatingAdmissionWebhook"
